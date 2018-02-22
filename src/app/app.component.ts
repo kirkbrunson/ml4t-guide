892bf072f1aca2e53f03752146d9f13307f1bdfe
Kirk Brunson <github@kirkbrunson.com>
2018-02-22 11:15:22 -0500

    feat: added main Q&A functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as questions from '../assets/ml4t_questions.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  currentQuestion: any;
+  questionIndex: number = 0;
+  displayAnswer: boolean = false;
+  answerStatus: string = 'Show';
+
+  ngOnInit() {
+  	this.currentQuestion = questions[this.questionIndex];
+  	console.log(this.currentQuestion);
+  }
+
+  nextQuestion() {
+  	this.questionIndex += 1;
+  	this.getQuestion(this.questionIndex);
+  }
+
+  getRandomQuestion() {
+    this.questionIndex = Math.floor(Math.random() * Math.floor(questions.length));
+    this.getQuestion(this.questionIndex);
+  }
+
+  getQuestion(index: number) {
+	  // TODO: display errmsg if out of bounds
+    if (index < 0 || index >= questions.length) {
+      this.questionIndex = 0;
+    }
+  	
+  	this.questionIndex = index;
+  	this.currentQuestion = questions[index]
+  	this.displayAnswer = false;
+    this.answerStatus = this.displayAnswer ? 'Hide' : 'Show';
+  }
+
+  toggleAnswer() {
+  	this.displayAnswer = !this.displayAnswer;
+  	this.answerStatus = this.displayAnswer ? 'Hide' : 'Show';
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
